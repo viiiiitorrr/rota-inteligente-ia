d36f7d344294b95290c51ac7b73ba2a20f4ee015
viiiiitorrr <vn533612@gmail.com>
2026-03-14 22:33:16 -0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("Sistema de Rotas Inteligentes iniciado")
+
+# exemplo simples de pontos de entrega
+pontos_entrega = ["Centro", "Bairro A", "Bairro B", "Bairro C"]
+
+print("Pontos de entrega cadastrados:")
+for ponto in pontos_entrega:
+    print("-", ponto)
